c4456920566cd96435af73fe85840fd55d4d4019
Benedetta-Barbone <barbone1912@gmail.com>
2024-05-19 15:42:39 +0200

    aggiunta griglia in html e inserite celle in js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
+const griglia = document.querySelector('.griglia');
 
+for (index=0; index<100 ; index++){
+    let cella = document.createElement('griglia');
+    cella.classList.add('cella');
+    griglia.appendChild(cella);
+}
