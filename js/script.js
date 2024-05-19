3d283af870787dd0e6dccf651add1df4b8bd1423
Benedetta-Barbone <barbone1912@gmail.com>
2024-05-19 20:53:22 +0200

    inseriti numeri all'interno delle celle e classe active

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 const griglia = document.querySelector('.griglia');
 
 for (index=0; index<100 ; index++){
-    let cella = document.createElement('griglia');
+    let cella = document.createElement('div');
     cella.classList.add('cella');
     griglia.appendChild(cella);
+
+    let numeroCella = document.createElement('span');
+    cella.appendChild(numeroCella);
+    numeroCella.append(index + 1);
+    
+    cella.addEventListener("click", function() {
+        cella.classList.add('active')
+        console.log("Hai cliccato sulla cella numero:", numeroCella);
+    });
 }
